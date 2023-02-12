15fcf0805de2bd3f3bec7ac66bff1fb77960b4da
rafalou38 <rafael.martinezcalvo@orange.fr>
2023-02-12 10:38:17 +0100

    [capture] 💥 Add collision ⌨️ Disable keyboard 🐌 Slower troops

--- src/lib/games/capture/Troop.ts
+++ src/lib/games/capture/Troop.ts
@@ -1,5 +1,5 @@
 import { currentConnection } from '$lib/api/connection';
-import type { Vector2 } from '$lib/utils/math';
+import { Vector2 } from '$lib/utils/math';
 import {
 	Application,
 	Graphics,
@@ -13,12 +13,14 @@ import { Blob } from './Blob';
 import { GRAY, GREEN, ownerMap, RED, WHITE, type Owner } from './utils';
 
 let canvasFactor = 1;
+const baseRadius = 0.05;
 let grSelf: RenderTexture;
 let grOther: RenderTexture;
 export class Troop {
 	static otherContainer: ParticleContainer;
 	static selfContainer: ParticleContainer;
 	static troops: Troop[] = [];
+	static troopsMap: Map<number, Troop> = new Map();
 	static app: Application;
 
 	static Setup(app: Application) {
@@ -37,13 +39,13 @@ export class Troop {
 
 		gr.clear();
 		gr.beginFill(get(currentConnection)?.isHost ? GREEN : RED);
-		gr.drawCircle(0, 0, 0.05 * canvasFactor);
+		gr.drawCircle(0, 0, baseRadius * canvasFactor);
 		gr.endFill();
 		grSelf = this.app.renderer.generateTexture(gr);
 
 		gr.clear();
 		gr.beginFill(get(currentConnection)?.isHost ? RED : GREEN);
-		gr.drawCircle(0, 0, 0.05 * canvasFactor);
+		gr.drawCircle(0, 0, baseRadius * canvasFactor);
 		gr.endFill();
 		grOther = this.app.renderer.generateTexture(gr);
 	}
@@ -60,19 +62,27 @@ export class Troop {
 		}
 	}
 
+	id: number;
+
 	pos: Vector2;
 	sprite: Sprite;
 	owner: Owner;
+	destroyed = false;
 
 	target: Blob;
 	remote: boolean;
 
-	constructor(origin: Blob, target: Blob, remote = false) {
+	constructor(origin: Blob, target: Blob, remote = false, pos?: Vector2, id?: number) {
+		this.id = id || Math.random();
+		Troop.troopsMap.set(this.id, this);
 		this.remote = remote;
-		this.pos = origin.pos.copy();
-		this.pos.x += Math.random() * Blob.baseRadius - Blob.baseRadius;
-		this.pos.y += Math.random() * Blob.baseRadius - Blob.baseRadius;
-
+		if (pos) {
+			this.pos = pos.sym(new Vector2(0, 10), new Vector2(10, 0));
+		} else {
+			this.pos = origin.pos.copy();
+			this.pos.x += Math.random() * Blob.baseRadius - Blob.baseRadius;
+			this.pos.y += Math.random() * Blob.baseRadius - Blob.baseRadius;
+		}
 		this.owner = origin.owner;
 		this.target = target;
 
@@ -94,13 +104,15 @@ export class Troop {
 					y: this.pos.y,
 					targetID: target.id,
 					originID: origin.id,
+					// id: this.id,
 				},
 			});
 	}
 
 	update(dt: number) {
+		if (this.destroyed) return;
 		const between = this.pos.to(this.target.pos);
-		const movement = between.scale(1 / between.norm()).scale(dt / 700);
+		const movement = between.scale(1 / between.norm()).scale(dt / 1000);
 		this.pos.add(movement);
 
 		if (this.target.pos.to(this.pos).norm() < Blob.baseRadius) {
@@ -108,9 +120,29 @@ export class Troop {
 			if (!this.remote) this.target.receive(this);
 			this.destroy();
 		}
+
+		if (this.owner == 'self') {
+			const destroyed = new Set<number>();
+			for (const other of Troop.troops) {
+				if (other.owner != this.owner && !other.destroyed && !this.destroyed) {
+					const dist = this.pos.to(other.pos).norm();
+
+					if (dist < baseRadius * 2) {
+						this.destroyed = true;
+						other.destroyed = true;
+						destroyed.add(this.id);
+						destroyed.add(other.id);
+					}
+				}
+			}
+
+			destroyed.forEach((d) => Troop.troopsMap.get(d)?.destroy());
+		}
 	}
 
 	draw() {
+		if (this.destroyed) return;
+
 		if (this.owner === 'other') this.sprite.texture = grOther;
 		else this.sprite.texture = grSelf;
 
@@ -118,6 +150,7 @@ export class Troop {
 	}
 
 	destroy() {
+		this.destroyed = true;
 		Troop.troops = Troop.troops.filter((t) => t != this);
 		if (this.owner === 'other') {
 			Troop.otherContainer.removeChild(this.sprite);
